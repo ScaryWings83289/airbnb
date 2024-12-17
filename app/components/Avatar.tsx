bc5fea0a04363184a00331c660eaf826f38f51bb
Divyankar Bhargav <divyankar.bhargav@simpplr.com>
2024-12-17 19:13:59 +0530

    feat: setup initial project, added tailwind library and navbar component

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+//* Packages Imports */
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      className='rounded-full'
+      height='30'
+      width='30'
+      alt='Avatar'
+      src='/images/avatar.png'
+    />
+  );
+};
+
+export default Avatar;
