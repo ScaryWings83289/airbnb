8549f359527dcd8a33648d07d8e12bad0ca9e7eb
Divyankar Bhargav <divyankar.bhargav@simpplr.com>
2025-01-19 12:05:57 +0530

    feat: added my reservations screen and fetching data with server components

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,36 @@
+//* Packages Imports */
+import { NextResponse } from "next/server";
+
+//* Utils Imports */
+import prisma from "@/app/libs/prismadb";
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const reservations = await prisma.reservations.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
+    },
+  });
+
+  return NextResponse.json(reservations);
+}
---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+//* Packages Imports */
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+
+//* Components Imports */
+import Container from "@/app/components/Container";
+import Heading from "@/app/components/Heading";
+import ListingCard from "@/app/components/listings/ListingCard";
+
+//* Utils Imports */
+import { SafeReservations, SafeUser } from "@/app/types";
+
+interface ReservationsClientProps {
+  reservations: SafeReservations[];
+  currentUser: SafeUser | null;
+}
+
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("Reservation cancelled!");
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error);
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading title='Reservations' subtitle='Bookings on your properties' />
+      <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-8'>
+        {reservations.map((reservation: SafeReservations) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel='Cancel guest reservation'
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default ReservationsClient;
